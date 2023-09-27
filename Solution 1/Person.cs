1d425c9f1bdf763652684bceddc1c889e522461e
AleksandrMatskevich <kokoseke06@gmail.com>
2023-09-27 11:39:21 +0300

    1 задание

---
+++ Solution 1/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        private string address;
+
+        public void SetName(string name = "")
+        {
+            this.name = name;
+        }
+        public void SetAge(int age = 0)
+        {
+            this.age = age;
+        }
+        public void SetAddress(string address = "-")
+        {
+            this.address = address;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public int GetAge()
+        {
+            return this.age;
+        }
+        public string GetAddress()
+        {
+            return this.address;
+        }
+    }
+}
--- Solution 1/Program.cs
+++ Solution 1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Solution_1
 {
@@ -10,7 +7,50 @@ namespace Solution_1
     {
         static void Main(string[] args)
         {
-
+            string sLen = "";
+            int iLen = 0;
+            do
+            {
+                Console.WriteLine("Введите количество персон");
+                sLen = Console.ReadLine();
+            }
+            while (!int.TryParse(sLen, out iLen));
+            Person[] person = new Person[iLen];
+            Console.WriteLine();
+            for (int i = 0; i < person.Length; i++)
+            {
+                string pattern = @"^[А-ЯЁ][а-яё]*$";
+                string sName = "";
+                string sAge = "";
+                int iAge = 0;
+                person[i] = new Person();
+                do
+                {
+                    Console.WriteLine($"Введите имя персонажа {i + 1}");
+                    sName = Console.ReadLine();
+                }
+                while (!Regex.IsMatch(sName, pattern));
+                person[i].SetName(sName);
+                do
+                {
+                    Console.WriteLine($"Введите возраст персонажа {i + 1}");
+                    sAge = Console.ReadLine();
+                }
+                while (!int.TryParse(sAge, out iAge));
+                person[i].SetAge(iAge);
+                Console.WriteLine($"Введите адрес персонажа {i + 1}");
+                person[i].SetAddress(Console.ReadLine());
+                Console.WriteLine();
+            }
+            Console.Clear();
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.WriteLine($"Person-{i + 1}");
+                Console.WriteLine($"Имя: {person[i].GetName()}");
+                Console.WriteLine($"Возраст: {person[i].GetAge()}");
+                Console.WriteLine($"Адрес: {person[i].GetAddress()}");
+                Console.WriteLine();
+            }
         }
     }
 }
